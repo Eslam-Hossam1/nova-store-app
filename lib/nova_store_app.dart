0b47e5df66acece592253ca471c2585c2233e584
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-18 06:02:06 +0300

    refactor: extract MyApp to separate file and rename to NovaStoreApp

---
+++ lib/nova_store_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NovaStoreApp extends StatelessWidget {
+  const NovaStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nova_store_app/main.dart';
+import 'package:nova_store_app/nova_store_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NovaStoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
