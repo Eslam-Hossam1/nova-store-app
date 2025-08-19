c9f7a713802c3b37b510367be7de6e3dac259cf0
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-19 15:18:49 +0300

    feat: add inter font to material app

--- lib/nova_store_app.dart
+++ lib/nova_store_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nova_store_app/core/routing/app_router.dart';
 
 class NovaStoreApp extends StatelessWidget {
@@ -13,6 +14,10 @@ class NovaStoreApp extends StatelessWidget {
       splitScreenMode: true,
       child: MaterialApp.router(
         routerConfig: AppRouter.router,
+        theme: ThemeData(
+          textTheme: GoogleFonts.interTextTheme(),
+          scaffoldBackgroundColor: Colors.white,
+        ),
       ),
     );
   }
