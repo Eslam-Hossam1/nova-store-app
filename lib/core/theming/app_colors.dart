9cfcd705d0206679387ff6b2b481db73c04b813d
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-19 15:06:29 +0300

    chore: create app colors class

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const List<Color> gradi = <Color>[
+    Color(0xff845fa1),
+    Color(0xff34283e),
+  ];
+
+  static const Color dark = Color(0xff34283e);
+
+  static const Color gray = Color(0xff9b9b9b);
+
+  static const Color yellow = Color(0xffe7b944);
+
+  static const Color darkGray = Color(0xff605a65);
+
+  static const Color red = Color(0xffce3e3e);
+
+  static const Color grayLight = Color(0xffe1e1e1);
+
+  static const Color white = Color(0xffffffff);
+
+  static const Color green = Color(0xff46ab62);
+
+  static const List<Color> gradired = <Color>[
+    Color(0xffd23a3a),
+    Color(0xfff49763),
+  ];
+
+  static const Color bg = Color(0xfff4f3f4);
+}
