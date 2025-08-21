36740490df2f1a9598f3c618fec1fc7d1c7b486b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-21 18:33:06 +0300

    feat(onboarding): create onboarding colors class

---
+++ lib/features/onboarding/presentation/resources/onboarding_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class OnboardingColors {
+    static const Color girlForgroundContainer = Color(0xff2A034B);
+    static const Color mediaTopPadding = Color(0xff8E7AA1);
+}
