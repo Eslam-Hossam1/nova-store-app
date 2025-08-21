e325856793243ecf5d94b07e74c898fdd97bacf1
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-21 08:56:11 +0300

    feat(onboarding): create onboarding view body

--- lib/features/onboarding/presentation/views/onboarding_view.dart
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nova_store_app/features/onboarding/presentation/views/widgets/onboarding_view_body.dart';
 
 class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
@@ -7,9 +8,7 @@ class OnboardingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text('Onboarding Screen'),
-        ),
+        child: OnboardingViewBody(),
       ),
     );
   }
