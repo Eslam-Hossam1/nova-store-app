9b76b4b0b6733abd98ba0d59f3087f9f9066727b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-21 16:04:29 +0300

    feat(onboarding): replace onboarding place holder view with onboarding view in onboarding route

--- lib/core/routing/app_routes/onboarding_route.dart
+++ lib/core/routing/app_routes/onboarding_route.dart
@@ -1,16 +1,11 @@
-
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nova_store_app/core/routing/routes_paths.dart';
+import 'package:nova_store_app/features/onboarding/presentation/views/onboarding_view.dart';
 
 class OnboardingRoute {
   static GoRoute onboarding = GoRoute(
     path: RoutePaths.onboarding,
-    builder: (context, state) => const Scaffold(
-      body: Center(
-        child: Text('Onboarding Screen'),
-      ),
-    ),
+    builder: (context, state) => const OnboardingView(),
   );
 
   static List<GoRoute> routes = [
