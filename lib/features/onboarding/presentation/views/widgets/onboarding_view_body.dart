1d66ff319fd5e7bc7a18f7baa81ab43ce8096e7e
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-21 18:33:46 +0300

    feat(onboarding): create onboarding girl widget

--- lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nova_store_app/core/theming/app_colors.dart';
+import 'package:nova_store_app/core/utils/assets.dart';
+import 'package:nova_store_app/features/onboarding/presentation/resources/onboarding_colors.dart';
+import 'package:nova_store_app/features/onboarding/presentation/views/widgets/onboarding_girl.dart';
 
 class OnboardingViewBody extends StatelessWidget {
   const OnboardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          OnboardingGirl(),
+        ],
+      ),
+    );
   }
 }
