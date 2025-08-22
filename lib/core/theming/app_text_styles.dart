03e99a1ad546cf82cba8b1d08fb1605486898225
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-22 21:26:38 +0300

    refactor: remove unsed import in app_text_styles.dart

--- lib/core/theming/app_text_styles.dart
+++ lib/core/theming/app_text_styles.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nova_store_app/core/utils/size_config.dart';
 
 abstract class AppTextStyles {
