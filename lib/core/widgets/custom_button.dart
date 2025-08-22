43f4e4228110c4d4aee162b8b9fc3bc73d1432b7
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-22 21:26:55 +0300

    feat: create custom  button widget

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nova_store_app/core/theming/app_colors.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    this.backgroundColor,
+    required this.child,
+    required this.onPressed,
+    this.width,
+    this.borderRadius,
+    this.verticalPadding,
+  });
+  final Color? backgroundColor;
+  final Widget child;
+  final VoidCallback onPressed;
+  final double? width;
+  final double? borderRadius;
+  final double? verticalPadding;
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 48.h,
+      ),
+      child: SizedBox(
+        width: width,
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: backgroundColor ?? AppColors.yellow,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(borderRadius ?? 8.r),
+            ),
+          ),
+          onPressed: onPressed,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: verticalPadding ?? 13.h,
+            ),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
