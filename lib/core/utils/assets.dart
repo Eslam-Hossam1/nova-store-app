a6276e030de3acb24ddae55660bcd0e54208c7dd
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-22 20:38:40 +0300

    feat: update assets class

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
   /// Assets for imagesPngsOnboardingGirl
   /// assets/images/pngs/onboarding_girl.png
-  static const String imagesPngsOnboardingGirl =
-      "assets/images/pngs/onboarding_girl.png";
+  static const String imagesPngsOnboardingGirl = "assets/images/pngs/onboarding_girl.png";
+
+  /// Assets for imagesSvgsNovaStore
+  /// assets/images/svgs/NovaStore.svg
+  static const String imagesSvgsNovaStore = "assets/images/svgs/NovaStore.svg";
 
   /// Assets for imagesSvgsShoppingCart
   /// assets/images/svgs/shopping_cart.svg
-  static const String imagesSvgsShoppingCart =
-      "assets/images/svgs/shopping_cart.svg";
+  static const String imagesSvgsShoppingCart = "assets/images/svgs/shopping_cart.svg";
 }
+
