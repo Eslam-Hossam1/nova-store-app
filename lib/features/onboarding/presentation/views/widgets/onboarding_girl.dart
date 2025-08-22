52d5bceb5db8eda6353d5324328643affaa14c28
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-23 00:25:08 +0300

    feat(onboarding): simplfy onbaording girl widget

--- lib/features/onboarding/presentation/views/widgets/onboarding_girl.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_girl.dart
@@ -1,31 +1,23 @@
-
 import 'package:flutter/material.dart';
+import 'package:nova_store_app/core/extensions/clamping.dart';
 import 'package:nova_store_app/core/utils/assets.dart';
-import 'package:nova_store_app/features/onboarding/presentation/resources/onboarding_colors.dart';
 
 class OnboardingGirl extends StatelessWidget {
   const OnboardingGirl({
     super.key,
+    required this.girlImageMinHeight,
+    required this.girlSizeRatio,
   });
-
+  final double girlImageMinHeight;
+  final double girlSizeRatio;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Image.asset(
-            Assets.imagesPngsOnboardingGirl,
-            height: MediaQuery.sizeOf(context).height * 0.68,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Container(
-          width: MediaQuery.sizeOf(context).width,
-          height: MediaQuery.sizeOf(context).height * 0.68,
-          color: OnboardingColors.girlForgroundContainer.withAlpha(80),
-        )
-      ],
+    return Image.asset(
+      Assets.imagesPngsOnboardingGirl,
+      height: (MediaQuery.sizeOf(context).height * girlSizeRatio).atLeast(
+        girlImageMinHeight,
+      ),
+      fit: BoxFit.cover,
     );
   }
 }
