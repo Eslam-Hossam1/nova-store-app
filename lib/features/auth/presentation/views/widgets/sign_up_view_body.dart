b39111b7379749416122e2ebc0ef91756d166e2e
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-29 23:15:02 +0300

    feat(sign-up): add SignUpForm to SignUpViewBody

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:nova_store_app/features/auth/presentation/views/widgets/auth_app_bar.dart';
+import 'package:nova_store_app/features/auth/presentation/views/widgets/sign_up_form.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [
-        AuthAppBar(
-          title: 'Sign Up',
-        )
-      ],
-    );
+    return SignUpForm();
   }
 }
