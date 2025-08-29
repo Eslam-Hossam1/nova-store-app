6a133319b42152be818af03f044de842d1db25f2
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-29 23:07:06 +0300

    feat: create custom text form filed

---
+++ lib/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nova_store_app/core/theming/app_colors.dart';
+import 'package:nova_store_app/core/theming/app_text_styles.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color? backgroundColor;
+  final String? Function(String?)? validator;
+  final void Function(String?)? onSaved;
+  const CustomTextFormField({
+     this.onSaved,
+    super.key,
+    this.contentPadding,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    required this.hintText,
+    this.isObscureText,
+    this.suffixIcon,
+    this.backgroundColor,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onSaved: onSaved,
+      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+        focusedBorder: focusedBorder ??
+            OutlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.purple,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(8.r),
+            ),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: AppColors.grayLight,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(8.r),
+            ),
+        hintStyle: hintStyle ??
+            AppTextStyles.regular14(context)
+                .copyWith(color: AppColors.darkGray),
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+      ),
+      obscureText: isObscureText ?? false,
+      style: AppTextStyles.regular14(context)
+          .copyWith(color: AppColors.darkPurple),
+      validator: validator,
+    );
+  }
+}
