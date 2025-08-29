df9b0abdee8d8209b0db8ea9679d9973d3c05645
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-29 23:17:07 +0300

    feat(login): create LoginView widget and Login route

--- lib/core/routing/app_routes/auth_routes.dart
+++ lib/core/routing/app_routes/auth_routes.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:nova_store_app/core/routing/routes_paths.dart';
+import 'package:nova_store_app/features/auth/presentation/views/login_view.dart';
 import 'package:nova_store_app/features/auth/presentation/views/sign_up_view.dart';
 
 class AuthRoutes {
@@ -7,8 +8,13 @@ class AuthRoutes {
     path: RoutePaths.signUp,
     builder: (context, state) => const SignUpView()
   );
+  static GoRoute login = GoRoute(
+    path: RoutePaths.login,
+    builder: (context, state) => const LoginView()
+  );
 
   static List<GoRoute> routes = [
     signUp,
+    login,
   ];
 }
