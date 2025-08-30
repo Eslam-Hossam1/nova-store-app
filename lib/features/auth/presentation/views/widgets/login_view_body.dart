140be22c75a00069668d6d85e7f75291ccc64993
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-30 11:23:34 +0300

    feat(login): add LoginForm to LoginViewBody

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:nova_store_app/features/auth/presentation/views/widgets/login_form.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const LoginForm();
   }
 }
