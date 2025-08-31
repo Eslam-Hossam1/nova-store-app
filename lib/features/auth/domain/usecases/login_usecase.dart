0161645222c1f48b907c6496c22d0295572ad07b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-31 16:58:41 +0300

    feat: apply changes in login and sign up  usecases due to modify usecase interface to accept failure type which extends failure class

--- lib/features/auth/domain/usecases/login_usecase.dart
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -1,15 +1,15 @@
 import 'package:dartz/dartz.dart';
-import 'package:nova_store_app/core/errors/failures.dart';
+import 'package:nova_store_app/core/errors/network_failure.dart';
 import 'package:nova_store_app/core/usecases/usecase.dart';
 import 'package:nova_store_app/features/auth/domain/repos/auth_repo.dart';
 
-class LoginUsecase implements Usecase<void, LoginParams> {
+class LoginUsecase implements Usecase<NetworkFailure,void, LoginParams> {
   final AuthRepo authRepo;
 
   LoginUsecase({required this.authRepo});
 
   @override
-  Future<Either<Failure, void>> call(LoginParams params) async {
+  Future<Either<NetworkFailure, void>> call(LoginParams params) async {
     return await authRepo.login(params.email);
   }
 }
