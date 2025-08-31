175a62cb77383efc308adba6392e264e3d886f86
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-08-31 16:56:49 +0300

    feat: modify base usecase interface to accept failure type which extends failure class

--- lib/core/usecases/no_param_usecase.dart
+++ lib/core/usecases/no_param_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:nova_store_app/core/errors/failures.dart';
 
-abstract interface class NoParamUsecase<T> {
-  Future<Either<Failure, T>> call();
+abstract interface class NoParamUsecase<FailureType extends Failure,T> {
+  Future<Either<FailureType, T>> call();
 }
--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:nova_store_app/core/errors/failures.dart';
 
-abstract interface  class Usecase<T, Params> {
-  Future<Either<Failure, T>> call(Params params);
+abstract interface class Usecase<FailureType extends Failure, T, Params> {
+  Future<Either<FailureType, T>> call(Params params);
 }
