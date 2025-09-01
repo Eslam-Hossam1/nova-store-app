543d306dae441229099e1c00e859285fd8e7e7b6
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 17:23:24 +0300

    feat(login): add LoginButtonBuilder to   LoginViewBody and add onSaved to EnterEmailSection in LoginViewBody

--- lib/features/auth/presentation/views/widgets/login/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login/login_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nova_store_app/core/utils/spacing.dart';
+import 'package:nova_store_app/features/auth/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:nova_store_app/features/auth/presentation/views/widgets/auth_app_bar.dart';
 import 'package:nova_store_app/features/auth/presentation/views/widgets/login/dont_have_an_account.dart';
 import 'package:nova_store_app/features/auth/presentation/views/widgets/enter_email_section.dart';
-import 'package:nova_store_app/features/auth/presentation/views/widgets/login/login_button.dart';
+import 'package:nova_store_app/features/auth/presentation/views/widgets/login/login_button_builder.dart';
 
 class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
@@ -35,7 +37,11 @@ class _LoginViewBodyState extends State<LoginViewBody> {
             padding: EdgeInsets.symmetric(
               horizontal: 24.w,
             ),
-            sliver: EnterEmailSection(),
+            sliver: EnterEmailSection(
+               onSaved: (email) {
+                context.read<LoginCubit>().email = email!;
+              },
+            ),
           ),
 
           SliverFillRemaining(
@@ -47,7 +53,7 @@ class _LoginViewBodyState extends State<LoginViewBody> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    LoginButton(
+                    LoginButtonBuilder(
                       formKey: formKey,
                       enableAutoValidation: enableAutoValidation,
                     ),
