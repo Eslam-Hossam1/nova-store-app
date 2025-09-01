a8a1e937f2e8cc5e0021b2d53815907b9a1fec31
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 18:45:23 +0300

    feat(otp): create OtpViewBody widget

--- lib/features/otp/presentation/views/otp_view.dart
+++ lib/features/otp/presentation/views/otp_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:nova_store_app/features/otp/presentation/views/widgets/otp_view_body.dart';
 
 class OtpView extends StatelessWidget {
   const OtpView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: OtpViewBody(),
+    );
   }
 }
