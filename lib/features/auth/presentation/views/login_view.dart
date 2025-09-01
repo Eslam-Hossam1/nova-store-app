6765adb70af097475160cffbe11443fda8749e70
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 17:24:09 +0300

    feat(sign-up): add bloc listner to LoginView  to handle actions performed for login states

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nova_store_app/core/helpers/dialog_helper.dart';
+import 'package:nova_store_app/features/auth/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:nova_store_app/features/auth/presentation/views/widgets/login/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -6,8 +9,20 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginFailure) {
+          DialogHelper.showErrorDialog(
+            context,
+            errorMessage: state.errMsg,
+          );
+        }else if (state is LoginSuccess) {
+          //Todo: navigate to verify email screen
+        }
+      },
+      child: Scaffold(
       body: LoginViewBody(),
+    ),
     );
   }
 }
