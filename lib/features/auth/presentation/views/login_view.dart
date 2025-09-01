78979ee34bf036e56a6c51775afca451adf9ead4
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 17:35:31 +0300

    feat(login): add NoInternetMixin for LoginView for Network Awareness

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_store_app/core/helpers/dialog_helper.dart';
+import 'package:nova_store_app/core/mixins/no_internet_mixin.dart';
 import 'package:nova_store_app/features/auth/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:nova_store_app/features/auth/presentation/views/widgets/login/login_view_body.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> with NoInternetMixin {
   @override
   Widget build(BuildContext context) {
     return BlocListener<LoginCubit, LoginState>(
