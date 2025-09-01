ecce8fc871a928f5d54d0f25ac3880ce23dca730
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 18:43:41 +0300

    feat(otp): create OtpView and assign it to otp route

--- lib/core/routing/app_routes/otp_route.dart
+++ lib/core/routing/app_routes/otp_route.dart
@@ -1,13 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nova_store_app/core/routing/routes_paths.dart';
+import 'package:nova_store_app/features/otp/presentation/views/otp_view.dart';
 
 class OtpRoute {
   static GoRoute otp = GoRoute(
     path: RoutePaths.otp,
-    builder: (context, state) => const Scaffold(
-      body: Center(child: Text('OTP view'),),
-    ),
+    builder: (context, state) => const OtpView(),
   );
 
   static List<GoRoute> routes = [
