6e7c39a9164230302f6b5a2595fd548f0229787b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 05:10:50 +0300

    feat(sign-up): disable AlreadyHaveAnAccount clicking while sign up loading state

--- lib/features/auth/presentation/views/widgets/already_have_an_account.dart
+++ lib/features/auth/presentation/views/widgets/already_have_an_account.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nova_store_app/core/routing/routes_paths.dart';
 import 'package:nova_store_app/core/theme/app_colors.dart';
 import 'package:nova_store_app/core/theme/app_text_styles.dart';
 import 'package:nova_store_app/core/widgets/clickable_text.dart';
+import 'package:nova_store_app/features/auth/presentation/manager/cubit/sign_up_cubit.dart';
 
 class AlreadyHaveAccount extends StatelessWidget {
   const AlreadyHaveAccount({
@@ -12,28 +14,35 @@ class AlreadyHaveAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Already have an account! ',
-          style: AppTextStyles.bold15(context).copyWith(
-            color: AppColors.darkGray,
+    return BlocBuilder<SignUpCubit, SignUpState>(
+      builder: (context, state) {
+        return AbsorbPointer(
+          absorbing: state is SignUpLoading,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Already have an account! ',
+                style: AppTextStyles.bold15(context).copyWith(
+                  color: AppColors.darkGray,
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              ClickableText(
+                onTap: () {
+                  context.go(RoutePaths.login);
+                },
+                text: 'Login',
+                style: AppTextStyles.bold15(context).copyWith(
+                  color: AppColors.lightPurple,
+                ),
+              ),
+            ],
           ),
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        ClickableText(
-          onTap: () {
-             context.go(RoutePaths.login);
-          },
-          text: 'Login',
-          style: AppTextStyles.bold15(context).copyWith(
-            color: AppColors.lightPurple,
-          ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
