ad002c723924dcb3648018c0cd88e95e3a00ce7c
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-01 05:18:42 +0300

    feat(sign-up): add BlocListener to SignUpView for handling SignUpCubit state changes

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nova_store_app/core/helpers/dialog_helper.dart';
+import 'package:nova_store_app/features/auth/presentation/manager/cubit/sign_up_cubit.dart';
 import 'package:nova_store_app/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -7,7 +10,7 @@ class SignUpView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const SignUpViewBody(),
+      body:  SignUpViewBody(),
     );
   }
 }
