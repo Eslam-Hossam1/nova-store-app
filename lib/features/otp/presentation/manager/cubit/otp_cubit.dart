270ecd37bdef6002302a83fefbb291a421d2a840
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-02 20:32:16 +0300

    feat(otp): create OtpCubit

---
+++ lib/features/otp/presentation/manager/cubit/otp_cubit.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nova_store_app/features/otp/domain/entities/otp_reason.dart';
+import 'package:nova_store_app/features/otp/domain/usecases/send_otp_usecase.dart';
+import 'package:nova_store_app/features/otp/domain/usecases/verify_otp_usecase.dart';
+
+part 'otp_state.dart';
+
+class OtpCubit extends Cubit<OtpState> {
+  final SendOtpUsecase _sendOtpUsecase;
+  final VerifyOtpUsecase _verifyOtpUsecase;
+  final OtpReason otpReason;
+
+  OtpCubit({
+    required VerifyOtpUsecase verifyOtpUsecase,
+    required SendOtpUsecase sendOtpUsecase,
+    required this.otpReason,
+  })  : _sendOtpUsecase = sendOtpUsecase,
+        _verifyOtpUsecase = verifyOtpUsecase,
+        super(OtpInitial());
+
+  Future<void> resendVerificationCode() async {
+    emit(OtpLoading());
+    var otpResult = await _sendOtpUsecase.call(
+      SendOtpParams(
+        otpReason: otpReason,
+      ),
+    );
+    otpResult.fold(
+      (failure) => emit(
+        OtpFailure(
+          errMsg: failure.errMsg,
+        ),
+      ),
+      (_) => emit(
+        SendOtpSuccess(),
+      ),
+    );
+  }
+
+  void showResendButton() => emit(OtpShowResendButton());
+
+  Future<void> verfiyVerificationCode({required String pinCode}) async {
+    emit(OtpLoading());
+    var otpResult = await _verifyOtpUsecase.call(
+      VerifyOtpParams(
+        pinCode: pinCode,
+        otpReason: otpReason,
+      ),
+    );
+
+    otpResult.fold(
+      (failure) => emit(
+        OtpFailure(
+          errMsg: failure.errMsg,
+        ),
+      ),
+      (_) => emit(
+        VerifyOtpSuccess(),
+      ),
+    );
+  }
+}
---
+++ lib/features/otp/presentation/manager/cubit/otp_state.dart
@@ -0,0 +1,19 @@
+part of 'otp_cubit.dart';
+
+
+sealed class OtpState {}
+
+final class OtpInitial extends OtpState {}
+
+final class OtpLoading extends OtpState {}
+
+final class OtpFailure extends OtpState {
+  final String errMsg;
+
+  OtpFailure({required this.errMsg});
+}
+
+final class OtpShowResendButton extends OtpState {}
+
+final class SendOtpSuccess extends OtpState {}
+final class VerifyOtpSuccess extends OtpState {}
