63a738bd97b376a0accd244efde522056aff7ec4
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-02 14:46:33 +0300

    feat(otp): create custom pin code field

---
+++ lib/core/widgets/custom_pin_code_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nova_store_app/core/theme/app_colors.dart';
+import 'package:nova_store_app/core/theme/app_text_styles.dart';
+import 'package:nova_store_app/core/utils/form_validators.dart';
+import 'package:nova_store_app/core/utils/size_config.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class CustomPinCodeField extends StatelessWidget {
+  const CustomPinCodeField(
+      {super.key, this.onChanged, this.onCompleted, this.onSaved});
+  final void Function(String)? onChanged;
+  final void Function(String)? onCompleted;
+  final void Function(String?)? onSaved;
+  @override
+  Widget build(BuildContext context) {
+    return PinCodeTextField(
+      autovalidateMode: AutovalidateMode.disabled,
+      onSaved: onSaved,
+      validator: FormValidators.pincodeTextFormFieldValidator,
+      pastedTextStyle: AppTextStyles.bold15(context).copyWith(
+        color: AppColors.darkPurple,
+      ),
+      appContext: context,
+      length: 6,
+      textStyle: AppTextStyles.textStyleSemiBold34(context)
+          .copyWith(color: AppColors.lightPurple),
+      showCursor: false,
+      animationType: AnimationType.fade,
+      keyboardType: TextInputType.number,
+      pinTheme: PinTheme(
+        shape: PinCodeFieldShape.box,
+        borderRadius: BorderRadius.circular(16.r),
+        fieldHeight: getfieldWidth(context),
+        fieldWidth: getfieldWidth(context),
+        activeColor: AppColors.lightPurple,
+        inactiveColor: AppColors.grayLight,
+        selectedColor: Colors.blue,
+      ),
+      onChanged: onChanged, // Optional for real-time input tracking
+      onCompleted: onCompleted, // Callback when the user completes input
+    );
+  }
+
+  double getfieldWidth(context) {
+    double width = MediaQuery.sizeOf(context).width;
+    return (48 * (width / SizeConfig.designWidth)).clamp(18, 60);
+  }
+}
