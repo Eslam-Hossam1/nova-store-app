6995b1cfc7a45612252afe9470d1cd39e4114cd2
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-02 20:31:57 +0300

    style(otp): move constructor to end of class in otp_reason.dart file

--- lib/features/otp/domain/entities/otp_reason.dart
+++ lib/features/otp/domain/entities/otp_reason.dart
@@ -4,8 +4,12 @@ abstract interface class OtpReason<T extends OtpResult> {
   final String verifyOtpEndpoint;
   final String sendOtpEndpoint;
   final String email;
-  OtpReason({required this.email ,required this.verifyOtpEndpoint, required this.sendOtpEndpoint});
   T resultBuilder(Map<String, dynamic> json);
-
   void onSuccess(T otpResult);
+
+  OtpReason({
+    required this.email,
+    required this.verifyOtpEndpoint,
+    required this.sendOtpEndpoint,
+  });
 }
