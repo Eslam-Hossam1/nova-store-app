d7e7080a904c24bb15213e76d85c2bef91799c4a
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-03 12:13:39 +0300

    feat(otp): add toGoViewPath field to OtpReason interface and make onSuccess return future

--- lib/features/otp/domain/entities/otp_reason.dart
+++ lib/features/otp/domain/entities/otp_reason.dart
@@ -4,10 +4,13 @@ abstract interface class OtpReason<T extends OtpResult> {
   final String verifyOtpEndpoint;
   final String sendOtpEndpoint;
   final String email;
+  final String toGoViewPath;
+
   T resultBuilder(Map<String, dynamic> json);
-  void onSuccess(T otpResult);
+  Future<void> onSuccess(T otpResult);
 
   OtpReason({
+    required this.toGoViewPath,
     required this.email,
     required this.verifyOtpEndpoint,
     required this.sendOtpEndpoint,
