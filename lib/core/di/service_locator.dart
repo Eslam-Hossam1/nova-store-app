1ffb05bf1e02aa937107911b1a09f8cf0b68e159
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-03 12:26:48 +0300

    feat(di): add setupCaching method to register caching related singltones and register OtpRepoImpl

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,20 +1,23 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:nova_store_app/core/api/dio_consumer.dart';
 import 'package:nova_store_app/core/api/network_connection_checker_impl.dart';
+import 'package:nova_store_app/core/auth/auth_credentials_manager.dart';
+import 'package:nova_store_app/core/auth/jwt_decoder/jwt_decoder_service_impl.dart';
+import 'package:nova_store_app/core/cache/secure_storage_helper.dart/secure_storage_helper.dart';
 import 'package:nova_store_app/core/cache/shared_pref/shared_prefernce_helper.dart';
 import 'package:nova_store_app/core/helpers/on_boarding_cache_helper.dart';
 import 'package:nova_store_app/features/auth/data/data_sources/auth_remote_data_source_impl.dart';
 import 'package:nova_store_app/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:nova_store_app/features/otp/data/data_sources/otp_remote_data_source_impl.dart';
+import 'package:nova_store_app/features/otp/data/repos/otp_repo_impl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 Future<void> setupServiceLocator() async {
-  final sharedPreferences = await SharedPreferences.getInstance();
-  getIt.registerSingleton<SharedPreferencesHelper>(
-    SharedPreferencesHelper(sharedPreferences),
-  );
+  await _setupCaching();
   getIt.registerSingleton<OnBoardingCacheHelper>(
     OnBoardingCacheHelper(
       sharedPreferencesHelper: getIt<SharedPreferencesHelper>(),
@@ -35,5 +38,37 @@ Future<void> setupServiceLocator() async {
       authRemoteDataSource:
           AuthRemoteDataSourceImpl(apiConsumer: getIt<DioConsumer>()),
     ),
+  );
+  getIt.registerSingleton<OtpRepoImpl>(
+    OtpRepoImpl(
+      otpRemoteDataSource:
+          OtpRemoteDataSourceImpl(apiConsumer: getIt<DioConsumer>()),
+    ),
+  );
+}
+
+Future<void> _setupCaching() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  getIt.registerSingleton<SharedPreferencesHelper>(
+    SharedPreferencesHelper(sharedPreferences),
+  );
+
+  AndroidOptions getAndroidOptions() => const AndroidOptions(
+        encryptedSharedPreferences: true,
+      );
+
+  final secureStorage = FlutterSecureStorage(
+    aOptions: getAndroidOptions(),
+  );
+
+  getIt.registerSingleton<SecureStorageHelper>(
+    SecureStorageHelper(secureStorage),
+  );
+
+  getIt.registerSingleton<AuthCredentialsManager>(
+    AuthCredentialsManager(
+        secureStorageHelper: getIt<SecureStorageHelper>(),
+        jwtDecoder: JwtDecoderServiceImpl()),
   );
 }
