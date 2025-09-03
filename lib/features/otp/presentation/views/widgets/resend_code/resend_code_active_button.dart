229bb0fc6b5be7e0dcd6216f05f46908550f9ee2
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-03 12:44:52 +0300

    feat(otp): invoke resendVerificationCode method in ResendCodeActiveButton's onTap method

--- lib/features/otp/presentation/views/widgets/resend_code/resend_code_active_button.dart
+++ lib/features/otp/presentation/views/widgets/resend_code/resend_code_active_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_store_app/core/theme/app_text_styles.dart';
+import 'package:nova_store_app/features/otp/presentation/manager/cubit/otp_cubit.dart';
 
 class ResendCodeActiveButton extends StatelessWidget {
   const ResendCodeActiveButton({super.key});
@@ -7,7 +9,9 @@ class ResendCodeActiveButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        context.read<OtpCubit>().resendVerificationCode();
+      },
       child: Text(
         'Resend code via email',
         style: AppTextStyles.semiBold14(context).copyWith(
