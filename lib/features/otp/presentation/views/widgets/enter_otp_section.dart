ff67fa492f766b0fd339b00af067fe9d5e0035d9
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-03 12:40:27 +0300

    feat(otp): show user email and save pinCode in CustomPinCodeField's onSaved method in EnterOtpSection widget

--- lib/features/otp/presentation/views/widgets/enter_otp_section.dart
+++ lib/features/otp/presentation/views/widgets/enter_otp_section.dart
@@ -1,15 +1,17 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nova_store_app/core/theme/app_colors.dart';
 import 'package:nova_store_app/core/theme/app_text_styles.dart';
 import 'package:nova_store_app/core/widgets/custom_pin_code_field.dart';
+import 'package:nova_store_app/features/otp/presentation/manager/cubit/otp_cubit.dart';
 
 class EnterOtpSection extends StatelessWidget {
   const EnterOtpSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final otpCubit = context.read<OtpCubit>();
     return SliverToBoxAdapter(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,7 +32,7 @@ class EnterOtpSection extends StatelessWidget {
                 ),
               ),
               TextSpan(
-                text: 'eslamhossam.tech@gmail.com',
+                text: otpCubit.otpReason.email,
                 style: AppTextStyles.regular14(context).copyWith(
                   color: AppColors.darkPurple,
                 ),
@@ -38,10 +40,14 @@ class EnterOtpSection extends StatelessWidget {
             ]),
           ),
           SizedBox(height: 32.h),
-          CustomPinCodeField(),
+          CustomPinCodeField(
+            
+            onSaved: (pinCode) {
+              otpCubit.pinCode = pinCode!;
+            },
+          ),
         ],
       ),
     );
-    ;
   }
 }
