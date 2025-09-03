e6bdca345de5383cd18364e79e1ab52db6c813b3
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-03 12:56:52 +0300

    feat(otp): finish OtpView widget
    - wrap it with bloc consumer, in listener handle actions should performed due to sate
    - wrap OtpView with ModalProgressHud to disable clicking while loading, all of that in builder
    - wrap OtpView with PopScope to warn user before popping OtpView

--- lib/features/otp/presentation/views/otp_view.dart
+++ lib/features/otp/presentation/views/otp_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:nova_store_app/core/helpers/dialog_helper.dart';
+import 'package:nova_store_app/features/otp/presentation/manager/cubit/otp_cubit.dart';
 import 'package:nova_store_app/features/otp/presentation/views/widgets/otp_view_body.dart';
 
 class OtpView extends StatelessWidget {
@@ -6,11 +11,39 @@ class OtpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: OtpViewBody(),
-      ),
+    final otpCubit = context.read<OtpCubit>();
+    return BlocConsumer<OtpCubit, OtpState>(
+      listener: (context, state) {
+        if (state is OtpFailure) {
+          DialogHelper.showErrorDialog(
+            context,
+            errorMessage: state.errMsg,
+            btnOkOnPress: () {},
+          );
+          otpCubit.showResendButton();
+        } else if (state is VerifyOtpSuccess) {
+          context.go(otpCubit.otpReason.toGoViewPath);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: state is OtpLoading,
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: PopScope(
+              onPopInvokedWithResult: (didPop, result) {
+                if (didPop) return;
+                DialogHelper.showCustomExitConfirmationDialog(
+                  context,
+                );
+              },
+              child: Scaffold(
+                body: OtpViewBody(),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
