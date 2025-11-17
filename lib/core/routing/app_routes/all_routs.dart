4fbcb2113b8487ceeb819bc06039ee7ee2b48cf8
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-11-17 20:42:27 +0200

    feat: add category route to app routes

--- lib/core/routing/app_routes/all_routs.dart
+++ lib/core/routing/app_routes/all_routs.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:nova_store_app/core/routing/app_routes/auth_routes.dart';
+import 'package:nova_store_app/core/routing/app_routes/category_route.dart';
 import 'package:nova_store_app/core/routing/app_routes/home_route.dart';
 import 'package:nova_store_app/core/routing/app_routes/onboarding_route.dart';
 import 'package:nova_store_app/core/routing/app_routes/otp_route.dart';
@@ -10,5 +11,6 @@ class AppRoutes {
     ...AuthRoutes.routes,
     ...OtpRoute.routes,
     HomeRoute.home,
+    CategoryRoute.category,
   ];
 }
